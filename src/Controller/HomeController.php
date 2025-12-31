4dcf349a971fa63ccfc2d4f4404b4a971447afef
Valerie Lavoisier <val.lavoisier3@gmail.com>
2025-12-31 23:42:52 +0100

    création de la page d'accueil, et de la page détail avec suppression voiture

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,49 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(VoitureRepository $voitureRepository): Response
     {
+        $voitures = $voitureRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'voitures' => $voitures,
         ]);
+    }
+
+    #[Route('/voiture/{id}', name: 'app_voiture_detail')]
+    public function detail(int $id, VoitureRepository $voitureRepository): Response
+    {
+        $voiture = $voitureRepository->find($id);
+        
+        if (!$voiture) {
+            throw $this->createNotFoundException('La voiture demandée n\'existe pas.');
+        }
+        
+        return $this->render('home/detail.html.twig', [
+            'voiture' => $voiture,
+        ]);
+    }
+
+    #[Route('/voiture/{id}/supprimer', name: 'app_voiture_delete')]
+    public function delete(int $id, VoitureRepository $voitureRepository, EntityManagerInterface $entityManager): Response
+    {
+        $voiture = $voitureRepository->find($id);
+        
+        if (!$voiture) {
+            return $this->redirectToRoute('app_home');
+        }
+        
+        $entityManager->remove($voiture);
+        $entityManager->flush();
+        
+        return $this->redirectToRoute('app_home');
     }
 }
